c3b66e5d3b5f64f56799270cec205023a1bdd40f
fiveinfinity <jlachance1@gmail.com>
2016-09-20 08:56:57 -0500

    added tests for WeatherService

--- tests/Routes.spec.js
+++ tests/Routes.spec.js
@@ -23,79 +23,24 @@ describe('Routes', function() {
     });
   });
 });
-//
-// describe('DateService', function() {
-//   beforeEach(module('app'));
-//   var DateService;
-//
-//   beforeEach(inject(function($injector) {
-//     DateService = $injector.get('DateService');
-//   }));
-//
-//   it('should should return month names', function() {
-//     var parsedDate = DateService.parseDate({data:{login: "nmuth", created_at: "2013-08-24T05:53:36Z"}});
-//     expect(parsedDate).toEqual('August 24, 2013');
-//   });
-// });
-//
-// describe('GithubService', function() {
-//   beforeEach(module('app'));
-//   var GitHubService, $httpBackend;
-//
-//   beforeEach(inject(function($injector) {
-//     GithubService = $injector.get('GithubService');
-//     $httpBackend = $injector.get('$httpBackend');
-//
-//     $httpBackend.when('GET', 'https://api.github.com/orgs/code42/members')
-//       .respond({login: 'fiveinfinity', repos: '300'});
-//     $httpBackend.when('GET', 'https://api.github.com/users/3/repos')
-//       .respond({login: 'fiveinfinity', repos: 'Code42-Github'});
-//     $httpBackend.when('GET', 'https://api.github.com/users/3')
-//       .respond({profile: 'fiveinfinity'});
-//     $httpBackend.when('GET', 'https://api.github.com/users/3/orgs')
-//       .respond({orgs: 'Code42'});
-//     $httpBackend.when('GET', 'https://api.github.com/repos/3/code42/stats/commit_activity')
-//       .respond({stats: '100 commits!'});
-//   }));
-//
-//   it('should get the members of Code42', function() {
-//     $httpBackend.expectGET('https://api.github.com/orgs/code42/members');
-//
-//     GithubService.getCode42Members().then(function(response) {
-//       expect(response.data.login).toEqual('fiveinfinity');
-//       expect(response.data.repos).toEqual('300');
-//     });
-//   });
-//
-//   it('should get members repos', function() {
-//     $httpBackend.expectGET('https://api.github.com/users/3/repos');
-//
-//     GithubService.getRepos().then(function(response) {
-//       expect(response.data.repos).toEqual('Code42-Github');
-//     });
-//   });
-//
-//   it('should get members profile', function() {
-//     $httpBackend.expectGET('https://api.github.com/users/3');
-//
-//     GithubService.getRepos().then(function(response) {
-//       expect(response.data.profile).toEqual('fiveinfinity');
-//     });
-//   });
-//
-//   it('should get members profile organizations', function() {
-//     $httpBackend.expectGET('https://api.github.com/users/3/orgs');
-//
-//     GithubService.getRepos().then(function(response) {
-//       expect(response.data.orgs).toEqual('Code42');
-//     });
-//   });
-//
-//   it('should get members stats', function() {
-//     $httpBackend.expectGET('https://api.github.com/repos/3/code42/stats/commit_activity');
-//
-//     GithubService.getRepos().then(function(response) {
-//       expect(response.data.stats).toEqual('100 commits!');
-//     });
-//   });
-// });
+
+describe('WeatherService', function() {
+  beforeEach(module('app'));
+  var WeatherService;
+
+  beforeEach(inject(function($injector) {
+    WeatherService = $injector.get('WeatherService');
+    $httpBackend = $injector.get('$httpBackend');
+
+    $httpBackend.when('GET', 'https://crossorigin.me/https://api.forecast.io/forecast/a56904ad029a4da12a16be1a4dc68cbe/41.111,42.222?UNITS=us')
+      .respond({weather: 'Super Cloudy'});
+  }));
+
+  it('should return the weather from Dark Sky', function() {
+    $httpBackend.expectGET('https://crossorigin.me/https://api.forecast.io/forecast/a56904ad029a4da12a16be1a4dc68cbe/41.111,42.222?UNITS=us');
+
+    WeatherService.getWeather().then(function(response) {
+      expect(response.data.weather).toEqual('Super Cloudy');
+    });
+  });
+});
